575e87a33a29d035d8cd36255142d3b6b8f95cde
Aniket Rajnish <58925008+aniketrajnish@users.noreply.github.com>
2023-11-14 15:21:31 -0500

    Collision Fix

--- 4D Game/Assets/Scripts/RaymarchCollision.cs
+++ 4D Game/Assets/Scripts/RaymarchCollision.cs
@@ -17,114 +17,119 @@ namespace Unity.Mathematics
         {
             raymarcher = Camera.main.GetComponent<Raymarcher>();
         }
-        /*private void Update()
+        private void Update()
         {
             MoveToGround();
             CheckRaymarchDist(bounds);
-        }*/
+        }
         public float GetShapeDistance(RaymarchRenderer rend, float3 p)
         {
             float wPos = raymarcher.wPos;
-            Vector3 wRot = raymarcher.wRot;
-            //Vector3 shapePos = rend.transform.position;
+            float3 wRot = raymarcher.wRot;
+            float3 shapePos = rend.transform.position;
+            float3 shapeRot = rend.transform.eulerAngles * Mathf.Deg2Rad;
 
-            float4 p4 = rotWposW(p, wPos, wRot);
+            p -= shapePos;
+
+            p.xz = mul(p.xz, float2x2(cos(shapeRot.y), sin(shapeRot.y), -sin(shapeRot.y), cos(shapeRot.y)));
+            p.yz = mul(p.yz, float2x2(cos(shapeRot.x), -sin(shapeRot.x), sin(shapeRot.x), cos(shapeRot.x)));
+            p.xy = mul(p.xy, float2x2(cos(shapeRot.z), -sin(shapeRot.z), sin(shapeRot.z), cos(shapeRot.z)));            
 
             vector12 dimensions = Helpers.GetDimensionVectors((int)rend.shape, rend.dimensions);
 
             switch (rend.shape)
             {
                 case RaymarchRenderer.Shape.Shpere:
-                    return DFs.sdSphere(p4.xyz, dimensions.a, wPos, wRot);
+                    return DFs.sdSphere(p, dimensions.a, wPos, wRot);
                 case RaymarchRenderer.Shape.Torus:
-                    return DFs.sdTorus(p4.xyz, new float2(dimensions.a, dimensions.b), wPos, wRot);
+                    return DFs.sdTorus(p, new float2(dimensions.a, dimensions.b), wPos, wRot);
                 case RaymarchRenderer.Shape.CappedTorus:
-                    return DFs.sdCappedTorus(p4.xyz, dimensions.a, dimensions.b, new float2(dimensions.c, dimensions.d), wPos, wRot);
+                    return DFs.sdCappedTorus(p, dimensions.a, dimensions.b, new float2(dimensions.c, dimensions.d), wPos, wRot);
                 case RaymarchRenderer.Shape.Link:
-                    return DFs.sdLink(p4.xyz, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
+                    return DFs.sdLink(p, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
                 case RaymarchRenderer.Shape.Plane:
-                    return DFs.sdPlane(p4.xyz, new float3(dimensions.a, dimensions.b, dimensions.c), dimensions.d, wPos, wRot);
+                    return DFs.sdPlane(p, new float3(dimensions.a, dimensions.b, dimensions.c), dimensions.d, wPos, wRot);
                 case RaymarchRenderer.Shape.Cone:
-                    return DFs.sdCone(p4.xyz, new float2(dimensions.a, dimensions.b), dimensions.c, wPos, wRot);
+                    return DFs.sdCone(p, new float2(dimensions.a, dimensions.b), dimensions.c, wPos, wRot);
                 case RaymarchRenderer.Shape.InfCone:
-                    return DFs.sdInfCone(p4.xyz, new float2(dimensions.a, dimensions.b), wPos, wRot);
+                    return DFs.sdInfCone(p, new float2(dimensions.a, dimensions.b), wPos, wRot);
                 case RaymarchRenderer.Shape.HexPrism:
-                    return DFs.sdHexPrism(p4.xyz, new float2(dimensions.a, dimensions.b), wPos, wRot);
+                    return DFs.sdHexPrism(p, new float2(dimensions.a, dimensions.b), wPos, wRot);
                 case RaymarchRenderer.Shape.TriPrism:
-                    return DFs.sdTriPrism(p4.xyz, new float2(dimensions.a, dimensions.b), wPos, wRot);
+                    return DFs.sdTriPrism(p, new float2(dimensions.a, dimensions.b), wPos, wRot);
                 case RaymarchRenderer.Shape.Capsule:
-                    return DFs.sdCapsule(p4.xyz, new float3(dimensions.a, dimensions.b, dimensions.c),
+                    return DFs.sdCapsule(p, new float3(dimensions.a, dimensions.b, dimensions.c),
                         new float3(dimensions.d, dimensions.e, dimensions.f),dimensions.g,
                         wPos, wRot);
                 case RaymarchRenderer.Shape.InfiniteCylinder:
-                    return DFs.sdInfiniteCylinder(p4.xyz, new float3(dimensions.a, dimensions.b, dimensions.c), wPos, wRot);
+                    return DFs.sdInfiniteCylinder(p, new float3(dimensions.a, dimensions.b, dimensions.c), wPos, wRot);
                 case RaymarchRenderer.Shape.Box:
-                    return DFs.sdBox(p4.xyz, dimensions.a, wPos, wRot);
+                    return DFs.sdBox(p, dimensions.a, wPos, wRot);
                 case RaymarchRenderer.Shape.RoundBox:
-                    return DFs.sdRoundBox(p4.xyz, dimensions.a, dimensions.b, wPos, wRot);
+                    return DFs.sdRoundBox(p, dimensions.a, dimensions.b, wPos, wRot);
                 case RaymarchRenderer.Shape.RoundedCylinder:
-                    return DFs.sdRoundedCylinder(p4.xyz, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
+                    return DFs.sdRoundedCylinder(p, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
                 case RaymarchRenderer.Shape.CappedCone:
-                    return DFs.sdCappedCone(p4.xyz, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
+                    return DFs.sdCappedCone(p, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
                 case RaymarchRenderer.Shape.BoxFrame:
-                    return DFs.sdBoxFrame(p4.xyz, new float3(dimensions.a, dimensions.b, dimensions.c), dimensions.d, wPos, wRot);
+                    return DFs.sdBoxFrame(p, new float3(dimensions.a, dimensions.b, dimensions.c), dimensions.d, wPos, wRot);
                 case RaymarchRenderer.Shape.SolidAngle:
-                    return DFs.sdSolidAngle(p4.xyz, new float2(dimensions.a, dimensions.b), dimensions.c, wPos, wRot);
+                    return DFs.sdSolidAngle(p, new float2(dimensions.a, dimensions.b), dimensions.c, wPos, wRot);
                 case RaymarchRenderer.Shape.CutSphere:
-                    return DFs.sdCutSphere(p4.xyz, dimensions.a, dimensions.b, wPos, wRot);
+                    return DFs.sdCutSphere(p, dimensions.a, dimensions.b, wPos, wRot);
                 case RaymarchRenderer.Shape.CutHollowSphere:
-                    return DFs.sdCutHollowSphere(p4.xyz, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
+                    return DFs.sdCutHollowSphere(p, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
                 case RaymarchRenderer.Shape.DeathStar:
-                    return DFs.sdDeathStar(p4.xyz, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
+                    return DFs.sdDeathStar(p, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
                 case RaymarchRenderer.Shape.RoundCone:
-                    return DFs.sdRoundCone(p4.xyz, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
+                    return DFs.sdRoundCone(p, dimensions.a, dimensions.b, dimensions.c, wPos, wRot);
                 case RaymarchRenderer.Shape.Ellipsoid:
-                    return DFs.sdEllipsoid(p4.xyz, new float3(dimensions.a, dimensions.b, dimensions.c), wPos, wRot);
+                    return DFs.sdEllipsoid(p, new float3(dimensions.a, dimensions.b, dimensions.c), wPos, wRot);
                 case RaymarchRenderer.Shape.Rhombus:
-                    return DFs.sdRhombus(p4.xyz, dimensions.a, dimensions.b, dimensions.c, dimensions.d, wPos, wRot);
+                    return DFs.sdRhombus(p, dimensions.a, dimensions.b, dimensions.c, dimensions.d, wPos, wRot);
                 case RaymarchRenderer.Shape.Octahedron:
-                    return DFs.sdOctahedron(p4.xyz, dimensions.a, wPos, wRot);
+                    return DFs.sdOctahedron(p, dimensions.a, wPos, wRot);
                 case RaymarchRenderer.Shape.Pyramid:
-                    return DFs.sdPyramid(p4.xyz, dimensions.a, wPos, wRot);
+                    return DFs.sdPyramid(p, dimensions.a, wPos, wRot);
                 case RaymarchRenderer.Shape.Triangle:
-                    return DFs.udTriangle(p4.xyz,
+                    return DFs.udTriangle(p,
                         new float3(dimensions.a, dimensions.b, dimensions.c),
                         new float3(dimensions.d, dimensions.e, dimensions.f),
                         new float3(dimensions.g, dimensions.h, dimensions.i),
                         wPos, wRot);
                 case RaymarchRenderer.Shape.Quad:
-                    return DFs.udQuad(p4.xyz,
+                    return DFs.udQuad(p,
                         new float3(dimensions.a, dimensions.b, dimensions.c),
                         new float3(dimensions.d, dimensions.e, dimensions.f),
                         new float3(dimensions.g, dimensions.h, dimensions.i),
                         new float3(dimensions.j, dimensions.k, dimensions.l),
                         wPos, wRot);
                 case RaymarchRenderer.Shape.Fractal:
-                    return DFs.sdFractal(p4.xyz,
+                    return DFs.sdFractal(p,
                         dimensions.a, dimensions.b, dimensions.c,
                         wPos, wRot);
                 case RaymarchRenderer.Shape.Tesseract:
-                    return DFs.sdTesseract(p4.xyz,
+                    return DFs.sdTesseract(p,
                         new float4(dimensions.a, dimensions.b, dimensions.c, dimensions.d),
                         wPos, wRot);
                 case RaymarchRenderer.Shape.HyperSphere:
-                    return DFs.sdHyperSphere(p4.xyz,
+                    return DFs.sdHyperSphere(p,
                         dimensions.a,
                         wPos, wRot);
                 case RaymarchRenderer.Shape.DuoCylinder:
-                    return DFs.sdDuoCylinder(p4.xyz,
+                    return DFs.sdDuoCylinder(p,
                         new float2(dimensions.a, dimensions.b),
                         wPos, wRot);
                 case RaymarchRenderer.Shape.VerticalCapsule:
-                    return DFs.sdVerticalCapsule(p4.xyz,
+                    return DFs.sdVerticalCapsule(p,
                         dimensions.a, dimensions.b,
                         wPos, wRot);
                 case RaymarchRenderer.Shape.FiveCell:
-                    return DFs.sdFiveCell(p4.xyz,
+                    return DFs.sdFiveCell(p,
                         new float4(dimensions.a, dimensions.b, dimensions.c, dimensions.d),
                         wPos, wRot);
                 case RaymarchRenderer.Shape.SixteenCell:
-                    return DFs.sdSixteenCell(p4.xyz,
+                    return DFs.sdSixteenCell(p,
                         dimensions.a,
                         wPos, wRot);
             }
@@ -133,26 +138,36 @@ namespace Unity.Mathematics
         }
         public float DistanceField(Vector3 p)
         {
+            float mod_x, mod_y, mod_z;
+
+            if (raymarcher.loop.x != 0)
+                mod_x = sdFMod(ref p.x, raymarcher.loop.x);
+            if (raymarcher.loop.y != 0)
+                mod_y = sdFMod(ref p.y, raymarcher.loop.y);
+            if (raymarcher.loop.z != 0)
+                mod_z = sdFMod(ref p.z, raymarcher.loop.z);
+
             float sigmaDist = Mathf.Infinity;
+
             foreach (var rend in raymarcher.renderers)
             {
                 float deltaDist = GetShapeDistance(rend, p);
                 switch (rend.operation)
                 {
                     case Operation.Union:
-                        sigmaDist = Mathf.Min(sigmaDist, deltaDist);
+                        sigmaDist = sdUnion(sigmaDist, deltaDist);
                         break;
                     case Operation.Intersect:
-                        sigmaDist = Mathf.Max(sigmaDist, deltaDist);
+                        sigmaDist = sdIntersection(sigmaDist, deltaDist);
                         break;
                     case Operation.Subtract:
-                        sigmaDist = Mathf.Max(sigmaDist, -deltaDist);
+                        sigmaDist = sdSubtraction(sigmaDist, deltaDist);
                         break;
                 }
             }
             return sigmaDist;
         }
-        /*void CheckRaymarchDist(Transform[] ro)
+        void CheckRaymarchDist(Transform[] ro)
         {
 
             for (int i = 0; i < ro.Length; i++)
@@ -184,7 +199,7 @@ namespace Unity.Mathematics
             d = Mathf.Min(d, maxMovement);
             //Debug.Log(d);
             transform.Translate(Vector3.down * d, Space.World);
-        }*/
+        }
 
     }
 }
